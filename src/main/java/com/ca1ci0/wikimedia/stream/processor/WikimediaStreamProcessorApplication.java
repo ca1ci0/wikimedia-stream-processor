44cdec7b90bec3de702b48de1556fa74f751ced0
ca1ci0 <mykolakoval1997@gmail.com>
2023-05-09 18:44:20 +0300

    initial structure

---
+++ src/main/java/com/ca1ci0/wikimedia/stream/processor/WikimediaStreamProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.ca1ci0.wikimedia.stream.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikimediaStreamProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikimediaStreamProcessorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ca1ci0/wikimedia/stream/processor/WikimediaStreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca1ci0.wikimedia.stream.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikimediaStreamProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
