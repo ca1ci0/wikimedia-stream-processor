0ab6ac93f8c2d8ca272de3633941d138dffe9549
ca1ci0 <mykolakoval1997@gmail.com>
2023-05-10 20:53:30 +0300

    fix grouping by user type

--- src/main/java/com/ca1ci0/wikimedia/stream/processor/processor/BotCountStreamProcessor.java
+++ src/main/java/com/ca1ci0/wikimedia/stream/processor/processor/BotCountStreamProcessor.java
@@ -3,13 +3,14 @@ package com.ca1ci0.wikimedia.stream.processor.processor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Map;
 
 @Component
@@ -22,9 +23,9 @@ public class BotCountStreamProcessor {
     @Autowired
     public void setup(StreamsBuilder streamsBuilder) {
         KStream<String, String> inputStream = streamsBuilder.stream("wikimedia.recentchange");
-
+        final TimeWindows timeWindows = TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1L));
         inputStream
-                .mapValues(changeJson -> {
+                .selectKey((k, changeJson) -> {
                     try {
                         final JsonNode jsonNode = OBJECT_MAPPER.readTree(changeJson);
                         if (jsonNode.get("bot")
@@ -36,17 +37,24 @@ public class BotCountStreamProcessor {
                         return "parse-error";
                     }
                 })
-                .groupBy((key, botOrNot) -> botOrNot)
+                .groupByKey()
+                .windowedBy(timeWindows)
                 .count(Materialized.as(BOT_COUNT_STORE))
                 .toStream()
                 .mapValues((key, value) -> {
-                    final Map<String, Long> kvMap = Map.of(String.valueOf(key), value);
+                    final Map<String, Object> kvMap = Map.of(
+                            "user_type", key.key(),
+                            "count", value
+                    );
                     try {
                         return OBJECT_MAPPER.writeValueAsString(kvMap);
                     } catch (JsonProcessingException e) {
                         return null;
                     }
                 })
-                .to(BOT_COUNT_TOPIC);
+                .to(BOT_COUNT_TOPIC, Produced.with(
+                        WindowedSerdes.timeWindowedSerdeFrom(String.class, timeWindows.size()),
+                        Serdes.String()
+                ));
     }
 }
